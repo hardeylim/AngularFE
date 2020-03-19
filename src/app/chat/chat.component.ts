5322ac41c6a1503c11ea5d58de8cc17815f6c662
Hardey <hardeylim@gmail.com>
2020-03-19 20:11:13 +0800

    Add Chat module

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.pug',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
