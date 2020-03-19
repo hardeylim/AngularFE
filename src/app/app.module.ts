07fc2ed65ab5663be7fa5147e55c8e23c3955bee
Hardey <hardeylim@gmail.com>
2020-03-20 04:25:09 +0800

    Create new thread list component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 import {AppRoutingModule} from './app-routing.module';
+import { ThreadListComponent } from './chat/thread-list/thread-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    ThreadListComponent
   ],
   imports: [
     AppRoutingModule,
---
+++ src/app/chat/thread-list/thread-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'thread-list',
+  templateUrl: './thread-list.component.pug',
+  styleUrls: ['./thread-list.component.css']
+})
+export class ThreadListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
