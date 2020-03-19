eb7119b7359e52f0972d59620fa8e8f1a0f385e3
Hardey <hardeylim@gmail.com>
2020-03-20 05:17:55 +0800

    Add message form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { ChatComponent } from './chat/chat.component';
 import {AppRoutingModule} from './app-routing.module';
 import { ThreadListComponent } from './chat/thread-list/thread-list.component';
 import { CurrentThreadComponent } from './chat/current-thread/current-thread.component';
+import { MessageFormComponent } from './chat/message-form/message-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
     ThreadListComponent,
-    CurrentThreadComponent
+    CurrentThreadComponent,
+    MessageFormComponent
   ],
   imports: [
     AppRoutingModule,
---
+++ src/app/chat/message-form/message-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'message-form',
+  templateUrl: './message-form.component.pug',
+  styleUrls: ['./message-form.component.css']
+})
+export class MessageFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
